22b22d3ff1452f210ac06916691f3a64bc86982b
Thomas Gak Deluen <tgdn45@gmail.com>
2016-03-12 12:15:33 +0000

    Deleted unused files

--- public/static/js/lib/app/housecreate.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom'
-import { Router, Route, Link, browserHistory } from 'react-router'
-
-import BaseComponent from './common/base-component.jsx';
-import LoginForm from './common/loginform.jsx';
-
-render(<LoginForm />, document.getElementById('app'));
